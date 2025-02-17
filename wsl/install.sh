92589bf19e9a892c0823518f510324ab07261494
shimosyan <nesher1121@yahoo.co.jp>
2025-02-17 11:15:17 +0900

    add change default shell on zsh

--- wsl/install.sh
+++ wsl/install.sh
@@ -4,6 +4,7 @@
 sudo apt install zsh
 git clone https://github.com/zplug/zplug $HOME/.zplug
 curl -sf https://raw.githubusercontent.com/shimosyan/setup/main/wsl/.zshrc -o $HOME/.zshrc
+chsh -s /bin/zsh
 
 ### git
 git config --global core.ignorecase false

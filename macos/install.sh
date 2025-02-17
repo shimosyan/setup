a733e6efc054a24de9c54328d49d65d8f4817cd5
shimosyan <nesher1121@yahoo.co.jp>
2025-02-17 18:37:49 +0900

    update

--- macos/install.sh
+++ macos/install.sh
@@ -14,12 +14,21 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 sudo chown -R $(whoami):admin /usr/local
 sudo chmod -R g+w /usr/local
 
+echo "Installing xcode-stuff"
 xcode-select --install
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> $HOME/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo >> $HOME/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+echo "Updating homebrew..."
+brew update
 
 brew install git
 brew install curl
@@ -41,17 +50,21 @@ brew install google-japanese-ime
 brew install google-backup-and-sync
 brew install postman
 
+echo "Cleaning up brew"
+brew cleanup
+
 ### git
 git config --global core.ignorecase false
 
 ### Anyenv
+echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(anyenv init -)"' >> ~/.zprofile
+eval "$(anyenv init -)"
+
 anyenv install --init
 anyenv install pyenv
 anyenv install nodenv
 anyenv install tfenv
-
-echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zprofile
-echo 'eval "$(anyenv init -)"' >> ~/.zprofile
 
 exec $SHELL -l
 
@@ -63,4 +76,4 @@ nodenv global 16.15.0
 curl -sf https://raw.githubusercontent.com/shimosyan/setup/main/macos/.zshrc -o ~/.zshrc
 
 ### Reboot
-reboot
+sudo shutdown -r now

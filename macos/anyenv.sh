1876e2060594ec69981a1c606b76307317be748d
shimosyan <nesher1121@yahoo.co.jp>
2025-05-21 14:24:22 +0900

    add golang and terraform

--- macos/anyenv.sh
+++ macos/anyenv.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Python
 pyenv install 3.9.5
+
+# Node.js
 nodenv install 22.14.0
 nodenv global 22.14.0
+
+# Terraform
+tfenv install latest
+tfenv use latest
+
+# Golang
+goenv install latest
+goenv global latest

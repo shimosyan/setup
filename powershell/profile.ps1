bfd83474a1686894bf15ee67cd104314e8558d59
shimosyan <owner@cube-unit.net>
2026-05-12 13:41:46 +0900

    feat: PowerShellプロファイルにPSReadLineの設定を追加
    - PSReadLineの履歴重複を防ぐオプションを設定
    - 履歴とプラグインを予測ソースとして設定
    - 予測ビューのスタイルをリストビューに設定

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -19,3 +19,10 @@ if (Test-Path Alias:ls) { Remove-Item Alias:ls -Force } # Remove ls Alias
 function ls { eza --icons --group-directories-first @args }
 function ll { eza -la --icons --group-directories-first --git @args }
 function lt { eza --tree --level=2 --icons @args }
+
+#
+# PSReadLine
+#
+Set-PSReadlineOption -HistoryNoDuplicates
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
